38c5ef54195d87018c4b05bd0b1cb0cc54b3e55e
Philip Hazelden <philip.hazelden@gmail.com>
2010-05-14 23:22:49 +0100

    Close window on quit.

--- src/space-game.cpp
+++ src/space-game.cpp
@@ -1,7 +1,26 @@
 #include <SFML/Graphics.hpp>
 
+sf::RenderWindow window;
+
+void processEvent(sf::Event e) {
+	switch (e.Type) {
+	case sf::Event::Closed:
+		window.Close();
+		break;
+	}
+}
+
 int main (int argv, char **argc) {
-	sf::RenderWindow window(sf::VideoMode(640, 480, 32), "Space shooter");
-	while (1)
+	window.Create(sf::VideoMode(640, 480, 32), "Space shooter");
+
+	while (window.IsOpened()) {
+		sf::Event ev;
+		while (window.GetEvent(ev))
+			processEvent(ev);
+
+		if (!window.IsOpened())
+			break;
+
 		window.Display();
+	}
 }
